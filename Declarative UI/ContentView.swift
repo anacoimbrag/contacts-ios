c9832916c22e5467db05a6b0d6d47212a47e9e88
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2020-06-25 18:58:00 -0300

    Initial Commit

---
+++ Declarative UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Declarative UI
+//
+//  Created by Ana Coimbra on 25/06/20.
+//  Copyright © 2020 Ana Coimbra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
