d62f21988d7d2683b98ea2fe501aa361ee89658e
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2020-06-28 12:38:37 -0300

    set navigation title

--- Contacts/Layout/ContentView.swift
+++ Contacts/Layout/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ContactListView()
-            .navigationBarTitle(Text("Contacts"))
+                .navigationBarTitle(Text("Contacts"))
         }
     }
 }
